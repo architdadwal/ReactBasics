c7555baf482d0ac84b5561ff361d128b0102cfcf
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-10-29 12:50:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <John />
+    </div>
+  );
+}
+
+function John() {
+  return (
+    <div>
+      {" "}
+      <h1> Hye i am archit </h1>{" "}
+    </div>
+  );
+}
+
+export default App;
